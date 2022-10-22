9a2f0041df5484f35612afdd28bdfd7d8aa8985d
thinkwithabs <twithabs@gmail.com>
2022-10-22 12:59:36 +0600

    home add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import Helmet from '../compononts/helmet/Helmet'
-
+import React from "react";
+import Helmet from "../compononts/helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from '../assets/images/hero-img.png'
+import '../styles/home.css'
+ 
 const Home = () => {
-  return <Helmet title={"home"}>
-    
-  </Helmet>
-}
+  const year = new Date().getFullYear();
 
-export default Home
+  return (
+    <Helmet title={"home"}>
+      <section className="hero_section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero_content">
+                <p className="sub_title">Trending product in {year}</p>
+                <h2>Make Your Interior More Minimalistic & Modern</h2>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Voluptatum enim harum facere esse aperiam dolores hic non
+                  tempora error inventore.
+                </p>
+                <div className="button_area">
+                  <button className="shop_button">Shop Now</button>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <div className="hero_img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Home;
