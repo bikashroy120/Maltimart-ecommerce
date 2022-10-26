d3bd9cb946b702c659c31e84f639a79138b30e47
thinkwithabs <twithabs@gmail.com>
2022-10-26 12:59:32 +0600

    product deteles add

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { Col, Container, Row } from 'reactstrap'
+import Helmet from '../compononts/helmet/Helmet'
+import CommongSection from '../compononts/UI/CommongSection'
+import {useParams} from 'react-router-dom'
+import products from "../assets/data/products";
+import { Rating } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import '../styles/productDeteles.css'
+
 
 const ProductDetails = () => {
+
+  const params = useParams()
+
+  const Product = products.find((item)=>item.id===params.id)
+  const {avgRating,category,description,imgUrl,price,productName,shortDesc,reviews}= Product
+  console.log(reviews)
+
+
+
   return (
-    <div>ProductDetails</div>
+    <Helmet title ="Product-Deteles">
+      <CommongSection title ="Product Deteles"/>
+      <section className='product_deteles pt-0'>
+        <Container>
+          <Row>
+            <Col lg="6" md="6"> 
+              <img src={imgUrl} alt="" />
+            </Col>
+            <Col lg="6" md="6">
+                <div className='product_deteles_content'>
+                    <h2 className='product_name'>{productName}</h2>
+                    <p className='short_dec'>{shortDesc}</p>
+                     <div className='retting'>
+                     <Rating name="read-only" value={avgRating} readOnly />
+                     <p>(<span>{avgRating}</span> ratings)</p>
+                     </div>
+                    <h3 className='price'>${price}</h3>
+                    <h5 className='category'>Category : {category}</h5>
+                    <div className='increment_box'>
+                      <button><RemoveIcon /></button>
+                      <span>2</span>
+                      <button><AddIcon /></button>
+                    </div>
+                    <button className='add_button'>Add to Cart</button>
+                </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,51 +1,94 @@
-import React from 'react'
-import Helmet from '../compononts/helmet/Helmet'
+import React, { useState } from "react";
+import Helmet from "../compononts/helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
-import CommongSection from '../compononts/UI/CommongSection';
-import '../styles/shop.css'
+import CommongSection from "../compononts/UI/CommongSection";
+import "../styles/shop.css";
+import products from "../assets/data/products";
+import ProductList from "../compononts/UI/ProductList";
 
 const Shop = () => {
+  const [productData, setproductData] = useState(products);
+
+
+  const catagoryFilter = (e)=>{
+    const filterItem = e.target.value;
+    if(filterItem==="all"){
+      setproductData(products)
+    }else{
+      const filterProduct = products.filter((item)=> item.category === filterItem)
+      setproductData(filterProduct)
+    }
+  }
+
+  const hendalSearch = (e) => {
+    const searchtrem = e.target.value;
+    const searchProduct = products.filter(
+      (item) =>
+        item.productName.toLowerCase().includes(searchtrem.toLowerCase()) ||
+        item.category.toLowerCase().includes(searchtrem.toLowerCase())
+    );
+    setproductData(searchProduct);
+  };
+
   return (
     <Helmet title={"shop"}>
-      <CommongSection title={"Product"}/>
-      <section className='shop_section'>
-          <Container>
-            <Row>
-              <Col lg='3'>
-                <div className='filter_widget'>
-                  <select>
-                    <option>Filter by Category</option>
-                    <option value="mobile">Mobile</option>
-                    <option value="sofa">Sofa</option>
-                    <option value="chair">Chair</option>
-                    <option value="watch">Watch</option>
-                    <option value="wireless">Wireless</option>
-                  </select>
-                </div>
-              </Col>
-              <Col lg='3'>
-              <div className='filter_widget'>
-                  <select>
-                    <option>Filter by Category</option>
-                    <option value="mobile">Mobile</option>
-                    <option value="sofa">Sofa</option>
-                    <option value="chair">Chair</option>
-                    <option value="watch">Watch</option>
-                    <option value="wireless">Wireless</option>
-                  </select>
-                </div>
-              </Col>
-              <Col lg='6'>
-                <div className='search_box'>
-                  <input type="text" placeholder='Search........'/>
-                  <span><i className='ri-search-line'></i></span>
-                </div>
-              </Col>
-            </Row>
-          </Container>
+      <CommongSection title={"Product"} />
+      <section className="shop_section">
+        <Container>
+          <Row>
+            <Col lg="3">
+              <div className="filter_widget">
+                <select onChange={catagoryFilter}>
+                  <option value={"all"}>Filter by Category</option>
+                  <option value="mobile">Mobile</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="3">
+              <div className="filter_widget">
+                <select>
+                  <option value={"all"}>Filter by Category</option>
+                  <option value="mobile">Mobile</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="search_box">
+                <input
+                  type="text"
+                  placeholder="Search........"
+                  onChange={hendalSearch}
+                />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {productData.length === 0 ? (
+              <h1>Product Not Found!..</h1>
+            ) : (
+              <ProductList products={productData} />
+            )}
+          </Row>
+        </Container>
       </section>
     </Helmet>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
